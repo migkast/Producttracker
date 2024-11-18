35760eba13ecdecf6911911105a25eaa9edf7b28
migkast <migkast@hotmail.com>
2024-11-18 19:04:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: { 
     unoptimized: true,
     domains: ['images.unsplash.com']
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
@@ -18,20 +23,6 @@ const nextConfig = {
         child_process: false,
       };
     }
-
-    // Add specific loader for undici
-    config.module.rules.push({
-      test: /node_modules\/undici\/.*\.js$/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-proposal-private-methods',
-          '@babel/plugin-proposal-class-static-block'
-        ]
-      }
-    });
 
     return config;
   },
